805fd13982ae64d4e03665ac161964a1f19b495f
ngocduy244 <87510332+ngocduy244@users.noreply.github.com>
2021-09-21 21:50:40 +0700

    update

--- js/back-scroll.js
+++ js/back-scroll.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     $("#top").click(function(){
         $("html, body").animate({
             scrollTop: 0
-        }, 1000);
+        }, 500);
     })
 
 })
--- js/change-img.js
+++ js/change-img.js
@@ -1,4 +1,91 @@
 function init(){
+    var images = document.querySelectorAll("div.products-2 img")
+    for(var i = 0; i < images.length; i++){
+        images[i].onclick = function(){
+            event.preventDefault()
+            var path = this.src
+            var img = document.getElementById("mainImg-2")
+            img.setAttribute("src", path)
+        }
+    }
+    var images = document.querySelectorAll("div.products-1 img")
+    for(var i = 0; i < images.length; i++){
+        images[i].onclick = function(){
+            event.preventDefault()
+            var path = this.src
+            var img = document.getElementById("mainImg-1")
+            img.setAttribute("src", path)
+        }
+    }
+
+    var images = document.querySelectorAll("div.products-3 img")
+    for(var i = 0; i < images.length; i++){
+        images[i].onclick = function(){
+            event.preventDefault()
+            var path = this.src
+            var img = document.getElementById("mainImg-3")
+            img.setAttribute("src", path)
+        }
+    }
+
+    var images = document.querySelectorAll("div.products-4 img")
+    for(var i = 0; i < images.length; i++){
+        images[i].onclick = function(){
+            event.preventDefault()
+            var path = this.src
+            var img = document.getElementById("mainImg-4")
+            img.setAttribute("src", path)
+        }
+    }
+
+    var images = document.querySelectorAll("div.products-5 img")
+    for(var i = 0; i < images.length; i++){
+        images[i].onclick = function(){
+            event.preventDefault()
+            var path = this.src
+            var img = document.getElementById("mainImg-5")
+            img.setAttribute("src", path)
+        }
+    }
+    var images = document.querySelectorAll("div.products-6 img")
+    for(var i = 0; i < images.length; i++){
+        images[i].onclick = function(){
+            event.preventDefault()
+            var path = this.src
+            var img = document.getElementById("mainImg-6")
+            img.setAttribute("src", path)
+        }
+    }
+    var images = document.querySelectorAll("div.products-7 img")
+    for(var i = 0; i < images.length; i++){
+        images[i].onclick = function(){
+            event.preventDefault()
+            var path = this.src
+            var img = document.getElementById("mainImg-7")
+            img.setAttribute("src", path)
+        }
+    }
+
+    var images = document.querySelectorAll("div.products-8 img")
+    for(var i = 0; i < images.length; i++){
+        images[i].onclick = function(){
+            event.preventDefault()
+            var path = this.src
+            var img = document.getElementById("mainImg-8")
+            img.setAttribute("src", path)
+        }
+    }
+
+    var images = document.querySelectorAll("div.products-9 img")
+    for(var i = 0; i < images.length; i++){
+        images[i].onclick = function(){
+            event.preventDefault()
+            var path = this.src
+            var img = document.getElementById("mainImg-9")
+            img.setAttribute("src", path)
+        }
+    }
+
     var images = document.querySelectorAll("div.thumb img")
     for(var i = 0; i < images.length; i++){
         images[i].onclick = function(){
@@ -8,6 +95,7 @@ function init(){
             img.setAttribute("src", path)
         }
     }
+
     var buttons = document.querySelectorAll(".btnColor a")
     for(var i = 0; i < buttons.length; i++){
         buttons[i].onclick = function(){
